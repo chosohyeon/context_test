88ae85dc830cb8679b28dcb68d2eacd85e3db035
hyeonkkyu <milktea185ml@naver.com>
2022-10-14 21:14:48 +0900

    22.10.14

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { createContext, useState } from 'react'
+import Sub from './Sub'
+import './dark.scss'
+
+const DarkTheme = createContext();
+export { DarkTheme }
 
 const App = () => {
+  const [dark, setDark] = useState(false)
   return (
-    <div>App</div>
+    <div className={`App ${dark ? 'on' : ''}`}>
+      <DarkTheme.Provider value={{ dark, setDark }}>
+        <Sub />
+      </DarkTheme.Provider>
+    </div>
   )
 }
 
